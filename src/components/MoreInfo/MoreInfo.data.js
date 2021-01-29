0e19f02d76a26f2a21545ec5ef80b006fb2c473a
ChukwuEbuka Beluolisa <developerebukab@gmail.com>
2021-01-29 09:56:25 -0500

    Pushed for demo

---
+++ src/components/MoreInfo/MoreInfo.data.js
@@ -0,0 +1,21 @@
+export const MORE_INFO_DATA = [
+  {
+    image: "./img/basket.jpeg",
+    title: "Our Life Basket Program",
+    text: "We deliver packages to low income, vulnerable or indigenous women every trimester. These baskets are filled with items and information intended to support them during pregnancy",
+    link: "/"
+  },
+  {
+    image: "./img/volunteer.jpeg",
+    title: "Volunteering Opportunities",
+    text: "We are currently recruiting: students, health professionals, and anyone who wants to make a difference for vulnerable women through the miracle of life process.",
+    link: "/"
+  },
+  {
+    image: "./img/community.jpeg",
+    title: "Online Community Network",
+    text: "Register and join the community. We aim to foster a supportive network for health professionals, mothers/to-be. Connect and share resources, information, and stories.",
+    link: "/"
+  }
+];
+
